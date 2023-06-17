5b012ca314e71616e9f0c47bcf2d1256dcb39079
oleh_ok <oleh.k@hotmail.com>
2023-06-17 17:42:29 +0300

    Post: get all posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return 'index';
+        return Post::all();
     }
 
     public function indexCache()
